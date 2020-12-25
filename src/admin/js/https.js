b502f16737904c3bae89dcc503081000bf99842a
White <1462449970@qq.com>
2020-12-25 22:29:59 +0800

    实现发表文章，文章类别管理

--- src/admin/js/https.js
+++ src/admin/js/https.js
@@ -1,18 +1,39 @@
 function myAjax(type, url, data, callback) {
-  $.ajax({
-    url: 'http://localhost:8080/api/v1' + url,
-    type: type,
-    headers: {
-      'Authorization': localStorage.getItem('token')
-    },
-    data: data,
-    success: function (resData) {
-      if (resData.code == 403) {
-        // window.parent.location = '../admin/login.html';
-        window.parent.location = './login.html';
-        return;
+  if (data instanceof FormData) {
+    $.ajax({
+      url: 'http://localhost:8080/api/v1' + url,
+      type: type,
+      headers: {
+        'Authorization': localStorage.getItem('token')
+      },
+      data: data,
+      contentType: false,
+      processData: false,
+      success: function (resData) {
+        if (resData.code == 403) {
+          // window.parent.location = '../admin/login.html';
+          window.parent.location = './login.html';
+          return;
+        }
+        callback(resData);
       }
-      callback(resData);
-    }
-  })
+    })
+  } else {
+    $.ajax({
+      url: 'http://localhost:8080/api/v1' + url,
+      type: type,
+      headers: {
+        'Authorization': localStorage.getItem('token')
+      },
+      data: data,
+      success: function (resData) {
+        if (resData.code == 403) {
+          // window.parent.location = '../admin/login.html';
+          window.parent.location = './login.html';
+          return;
+        }
+        callback(resData);
+      }
+    })
+  }
 }
